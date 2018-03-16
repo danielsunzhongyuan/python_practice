31b195abbbfa8c9699cf20f1e49e332e67b43e6e
sunzhongyuan <sunzhongyuan@lvwan.com>
2018-03-16 21:08:27 +0800

    大数据搜索引擎

---
+++ simple_search_engine/README.md
@@ -0,0 +1,2 @@
+# 用 Python 实现一个大数据搜索引擎
+http://python.jobbole.com/88921/
--- simple_search_engine/test_bloom_filter.py
+++ simple_search_engine/test_bloom_filter.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 
 from bloom_filter import Bloomfilter
+from splunk import Splunk, SplunkM
 
 
-def main():
+def bf_test():
     bf = Bloomfilter(10)
     bf.add_value('dog')
     bf.add_value('fish')
@@ -15,6 +16,41 @@ def main():
 
     for term in ['dog', 'fish', 'cat', 'bird', 'duck', 'emu']:
         print '{}: {} {}'.format(term, bf.hash_value(term), bf.might_contain(term))
+
+
+def splunk_test():
+    s = Splunk()
+    s.add_event('src_ip = 1.2.3.4')
+    s.add_event('src_ip = 5.6.7.8')
+    s.add_event('dst_ip = 1.2.3.4')
+
+    for event in s.search('1.2.3.4'):
+        print event
+    print '***********'
+    for event in s.search('src_ip'):
+        print event
+    print '***********'
+    for event in s.search('ip'):
+        print event
+
+def splunk_multi_test():
+    s = SplunkM()
+    s.add_event('src_ip = 1.2.3.4')
+    s.add_event('src_ip = 5.6.7.8')
+    s.add_event('dst_ip = 1.2.3.4')
+
+    for event in s.search_all(['src_ip', '5.6']):
+        print event
+    print '***********'
+    for event in s.search_any(['src_ip', 'dst_ip']):
+        print event
+
+def main():
+    bf_test()
+    print '#############\n'
+    splunk_test()
+    print '#############\n'
+    splunk_multi_test()
 
 
 if __name__ == "__main__":
