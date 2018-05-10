9225fcb7e67576020268ada303c7f777ea59b136
sunzhongyuan <sunzhongyuan@lvwan.com>
2018-05-10 14:50:52 +0800

    quick sort

---
+++ quick_sort/quick_sort.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+@author: willard
+'''
+
+def sub_sort(array,low,high):
+    key = array[low]
+    while low < high:
+        while low < high and array[high] >= key:
+            high -= 1
+        while low < high and array[high] < key:
+            array[low] = array[high]
+            low += 1
+            array[high] = array[low]
+    array[low] = key
+    return low
+
+
+def quick_sort1(array,low,high):
+    if low < high:
+        key_index = sub_sort(array,low,high)
+        quick_sort1(array,low,key_index-1)
+        quick_sort1(array,key_index+1,high)
+
+if __name__ == '__main__':
+    #array1 = [8,10,9,6,4,16,5,13,26,18,2,45,34,23,1,7,3]
+    #array1 = [7,3,5,6,2,4,1]
+    array1 = []
+
+    print array1
+    quick_sort1(array1,0,len(array1)-1)
+    print array1
